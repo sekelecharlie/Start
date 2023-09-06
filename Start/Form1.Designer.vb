9d5e547d7a477e2718d1ec8fd0d21f08a85ab033
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-06 11:53:55 +0200

    Add project files.

---
+++ Start/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnCalculator = New Button()
+        SuspendLayout()
+        ' 
+        ' btnCalculator
+        ' 
+        btnCalculator.Location = New Point(263, 111)
+        btnCalculator.Name = "btnCalculator"
+        btnCalculator.Size = New Size(222, 103)
+        btnCalculator.TabIndex = 0
+        btnCalculator.Text = "Calculator"
+        btnCalculator.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnCalculator)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnCalculator As Button
+End Class
---
+++ Start/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btnCalculator_Click(sender As Object, e As EventArgs) Handles btnCalculator.Click
+        Dim dPrice As Decimal
+        Dim dQuantity As Decimal
+        Dim dDiscount As Decimal
+        Dim dVat As Decimal
+        Dim dTotalCost As Decimal
+
+        dPrice = 25
+        dQuantity = 10
+        dDiscount = 2
+        dVat = 0.15
+        dTotalCost = (dPrice - dQuantity) * dDiscount
+
+        MsgBox("The total cost without vat for client is " & dTotalCost)
+    End Sub
+End Class
